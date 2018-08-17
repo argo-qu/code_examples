ff569e40ec338309cfe43bfa64eb32b7b6139083
argo-qu <afilipovich.qu@gmail.com>
2018-08-17 19:43:37 +0300

    Update map.ts

--- angular2/ionic2/map.ts
+++ angular2/ionic2/map.ts
@@ -2,13 +2,6 @@ import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 
-/**
- * Generated class for the MapPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 declare let ymaps: any;
 
 @IonicPage()
